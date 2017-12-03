e81b71ab5adf4aeeee46ca57fb9735a135669350
Frank Gutierrez <fgutie6@uic.edu>
2017-12-02 16:38:37 -0800

    Updated example json file name

--- yahooFinanceScraper/jsonreader.py
+++ yahooFinanceScraper/jsonreader.py
@@ -3,6 +3,6 @@ import os
 
 
 def get_tickers():
-    data_file = os.path.join(os.path.dirname(__file__), '../input/tickers.json')
+    data_file = os.path.join(os.path.dirname(__file__), '../input/exampletickers.json')
     data = json.load(open(data_file))
     return data
